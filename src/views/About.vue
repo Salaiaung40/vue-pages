08b36ef22c45b94048ebee98a40a50c2da627951
Salaiaung40 <salaihtunlin@hotnail.com>
2021-05-28 15:31:43 +0200

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsam autem
+      recusandae commodi, libero dolorum quia dolore blanditiis explicabo sint
+      voluptatibus necessitatibus voluptatum vero expedita omnis, a, porro
+      placeat earum!
+    </p>
+  </div>
+</template>
